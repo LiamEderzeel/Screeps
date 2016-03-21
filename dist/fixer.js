cd05ac7728bbea285bf0409099f4d53a4001083d
LiamEderzeel <liamederzeel@gmail.com>
2016-03-21 16:05:38 +0100

    current version

--- dist/fixer.js
+++ dist/fixer.js
@@ -4,40 +4,31 @@ module.exports = function (creep) {
             creep.moveTo(Game.spawns.Spawn1);               
         }
     } else {
-        var wallToRepair = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+        var targets = creep.room.find(FIND_STRUCTURES, {
             filter: function(object) {
                 if(object.structureType != STRUCTURE_ROAD ) {
                     return false;
                 }
-                if(object.hits >4900) {
+                if(object.hits > 4900) {
                 //if(object.hits > 100) {
                     return false;
                 }
                     return true;
                 } 
         });
-
-        if (wallToRepair) {
-            console.log("teat");
-            //creep.moveTo(roadToRepair);
-            //creep.repair(roadToRepair);
-            if(creep.repair(wallToRepair == ERR_NOT_IN_RANGE)) {
-                creep.moveTo(wallToRepair); 
-                console.log("teat");
+        
+        if (targets) {
+            targets.sort((a,b) => a.hits - b.hits);
+            //console.log(targets);
+            if(targets.length > 0) {
+                if(creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0]); 
+                    //console.log("test");
+                }
             }
         } else {
-            var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-            if(targets == 0)
-            {
-                if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE){
-                    creep.moveTo(creep.room.controller);
-                }
-            } else {
-                if(targets.length) {
-                    if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0]);                   
-                    }
-                }
+            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE){
+                creep.moveTo(creep.room.controller);
             }
         }
     } 
--- dist/harvester.js
+++ dist/harvester.js
@@ -8,7 +8,32 @@ module.exports = function (creep) {
 	}
 	else {
 		if(creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-			creep.moveTo(Game.spawns.Spawn1);
-		}			
+		    //Game.spawns.Spawn1.energy == !Game.spawns.Spawn1.energyCapacity
+		    creep.moveTo(Game.spawns.Spawn1);
+		}
+		else
+		{
+// 		    var extension = creep.pos.find(FIND_STRUCTURES, { filter: function (s) {
+//                 return s.structureType == STRUCTURE_EXTENSION
+//             }});
+			var extension = creep.room.find(FIND_STRUCTURES, { 
+				filter: function(object) {
+					if(object.structureType != STRUCTURE_EXTENSION ) {
+						return false;
+					}
+					if(object.energy === object.energyCapacity) {
+			            return false;
+			       	}
+			            return true;
+			    }
+			});
+			if(extension){
+			    console.log(extension);
+		        if(creep.transfer(extension[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+	                creep.moveTo(extension[0]);
+		        }
+			}
+
+		}
 	}
 }
